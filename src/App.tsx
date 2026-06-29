a60e884052c032fdf69311488dc2789d18bca12d
JoseAFD <joseafd@gmail.com>
2026-06-29 11:16:18 +0200

    Solucionar bug de visualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -255,7 +255,7 @@ export default function App() {
   };
 
   return (
-    <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }}>
+    <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
       
       {/* VIEW 1: HOME PAGE (PORTADA) */}
       {activeTab === 'home' && (
